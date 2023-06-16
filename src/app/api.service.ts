5096b4a2c931403430170bcaf0b2280e6578dda8
MangeshJ27 <mangesh.j@reapmind.com>
2023-06-16 20:41:53 +0530

    Interceptor

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,13 +1,26 @@
 import { Injectable } from '@angular/core';
-
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ApiService {
+export class ApiService implements HttpInterceptor {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private router: Router) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    const authReq = req.clone({});
+    return next.handle(authReq).pipe(
+      catchError(error => {
+        if (error.status === 408) {
+          this.router.navigate(['/login']);
+        }
+        return throwError(error);
+      })
+    );
+  }
 
   baseUrl = 'https://fakestoreapi.com/';
 
